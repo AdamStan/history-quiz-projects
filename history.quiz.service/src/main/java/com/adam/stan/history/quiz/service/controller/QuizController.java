bf69f30ef7556ab29c93474083555c262166b2d3
AdamStan <adam15stan@gmail.com>
2023-07-10 11:41:55 +0200

    adding api dependency to the service

---
+++ history.quiz.service/src/main/java/com/adam/stan/history/quiz/service/controller/QuizController.java
@@ -0,0 +1,16 @@
+package com.adam.stan.history.quiz.service.controller;
+
+import com.adam.stan.history.quiz.api.v1.QuizApi;
+import com.adam.stan.history.quiz.api.v1.model.Question;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public class QuizController implements QuizApi {
+    @Override
+    public ResponseEntity<List<Question>> getQuiz(Integer numberOfQuestions,
+                                                  Integer numberOfAnswers,
+                                                  List<String> categories) {
+        return null;
+    }
+}
